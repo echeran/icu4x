d779ba7d97b1f510ec0a9baf22add19c58f8a89c
github-action-benchmark <github@users.noreply.github.com>
2021-05-05 21:46:38 +0000

    add Rust Benchmark (cargo) benchmark result for 7cbe966099bd95ca509bb1d0c79ff2e5c114db5d

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1619110524255,
+  "lastUpdate": 1620251198835,
   "repoUrl": "https://github.com/echeran/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -35,6 +35,42 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 2066798,
             "range": "± 17917",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "7cbe966099bd95ca509bb1d0c79ff2e5c114db5d",
+          "message": "Add PR template, including reminder for Conventional Comments",
+          "timestamp": "2021-05-05T14:38:03-07:00",
+          "tree_id": "051e6ee6537ae39474bac4199c3ce5a1e2627bbc",
+          "url": "https://github.com/echeran/icu4x/commit/7cbe966099bd95ca509bb1d0c79ff2e5c114db5d"
+        },
+        "date": 1620251198233,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_overview",
+            "value": 1178403,
+            "range": "± 46432",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 2311929,
+            "range": "± 73697",
             "unit": "ns/iter"
           }
         ]
